38f4243a7dbb4633c1f2cf4d2769d3ee2968d181
EFTEKHER <eftekherali2000@gmail.com>
2024-04-12 01:31:55 +0600

    done

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -27,9 +27,9 @@ const Github = () => {
     },[])
   return (
     <div className='flex w-full h-[100vh] overflow-hidden '>
-       <div className='container flex flex-col w-[300px] h-[250px] justify-center rounded align-middle   gap-4  items-center m-auto bg-violet-200  transition-all duration-500  shadow-2xl transform hover:w-[350px] hover:h-[300px] '>
+       <div className='container flex flex-col lg:w-[600px] lg:h-[500px] md:w-[600px] md:h-[500px] sm:w-full sm:h-[90vh] justify-center rounded    gap-4  items-center m-auto bg-violet-200  transition-all duration-500  shadow-2xl transform lg:hover:w-[650px] lg:hover:h-[550px] md:hover:w-[650px] md:hover:h-[550px]  overflow-hidden '>
        <h3 className='text-center text-2xl font-bold text-black font-serif' >GitHub</h3>
-       <img className='h-20 w-20 rounded-full box-border' src={url} alt="" />
+       <img className='lg:h-20 lg:w-20 md:h-20 md:w-20 sm:h-5 sm:w-5 rounded-full box-border' src={url} alt="" />
         <h3 className=' text-black font-serif' >Name:{name}</h3>
         <h3 className=' left-3 text-black font-serif'>Follower:{follower}</h3>
        </div>
